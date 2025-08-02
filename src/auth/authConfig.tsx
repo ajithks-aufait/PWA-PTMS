8acdfd7b2e069453b47ac87a509fa1f065751ea2
ajith.k@aufaittechnologies.com <ajith.k@aufaittechnologies.com>
2025-08-02 15:13:41 +0530

    new url updated

--- src/auth/authConfig.tsx
+++ src/auth/authConfig.tsx
@@ -2,7 +2,8 @@ export const msalConfig = {
   auth: {
     clientId: "b71d2039-dadc-4393-8744-e7f648d085a1",
     authority: "https://login.microsoftonline.com/8efa5ce2-86e4-4882-840c-f2578cdf094c",
-    redirectUri: "http://localhost:5173",
+    redirectUri: "https://pwa-ptms.vercel.app"
+    // redirectUri: "http://localhost:5173",https://pwa-ptms.vercel.app/
   },
   cache: {
     cacheLocation: "localStorage",
