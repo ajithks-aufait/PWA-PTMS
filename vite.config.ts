d40106d569dd85467263c23414de68052960dd38
ajith.k@aufaittechnologies.com <ajith.k@aufaittechnologies.com>
2025-09-09 10:56:38 +0530

    pwa issue fixing

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      scope: './',
       strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'sw.ts',
@@ -26,17 +28,17 @@ export default defineConfig({
         background_color: '#FFFFFF',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: './',
+        start_url: './',
         icons: [
           {
-            src: '/icons/icon-192x192.png',
+            src: 'icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/icons/icon-512x512.png',
+            src: 'icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
