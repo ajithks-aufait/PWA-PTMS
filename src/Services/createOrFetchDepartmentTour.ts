ee53cb12ba7338d3660272149d1d5c1b80c36f96
ajith.k@aufaittechnologies.com <ajith.k@aufaittechnologies.com>
2025-10-07 12:13:46 +0530

    platform section added-1

--- src/Services/createOrFetchDepartmentTour.ts
+++ src/Services/createOrFetchDepartmentTour.ts
@@ -29,7 +29,6 @@ export interface DepartmentTourResponse {
   cr3ea_tourby: string;
   cr3ea_observedby: string;
   cr3ea_roleid: string;
-  cr3ea_platform?: string;
 }
 
 /**
@@ -82,7 +81,6 @@ export async function createOrFetchDepartmentTour({
     cr3ea_observedby: employeeName,
     cr3ea_roleid: userRoleID.toString(),
     cr3ea_title: title,
-    cr3ea_platform: "PWA",
   };
 
   try {
